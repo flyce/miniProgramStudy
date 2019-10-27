cca891ebc8ef90e21dcc6529fd6d310f02239aac
Echo <mail@flyce.cn>
2019-10-27 16:32:08 +0800

    完成流行页面

--- components/classic/music/index.js
+++ components/classic/music/index.js
@@ -1,26 +1,86 @@
 // components/music/index.js
 import { classicBeh } from '../classic-beh';
 
+const mMgr = wx.getBackgroundAudioManager();
+
 Component({
   behaviors: [classicBeh], 
   /**
    * 组件的属性列表
    */
   properties: {
+    src: String,
+    title: String
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-    pauseSrc: 'images/palyer@waitting.png',
-    playSrc: 'images/player@playing.png'
+    playing: false,
+    pauseSrc: 'images/player@pause.png',
+    playSrc: 'images/player@play.png'
+  },
+
+  attached: function() {
+    this._recoverStatus();
+    this._monitorSwitch()
+  },
+
+  detached: function() {
+    // mMgr.stop();
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
+    onPlay: function() {
+      if(!this.data.playing) {
+        this.setData({
+          playing: true
+        });
+        mMgr.src = this.properties.src;
+        mMgr.title = this.properties.title;
+      } else {
+        this.setData({
+          playing: false
+        });
+        mMgr.pause()
+      }
+    },
 
+    _recoverStatus: function() {
+      if(mMgr.paused) {
+        this.setData({
+          playing: false
+        });
+        return ;
+      }
+  
+      if(mMgr.src == this.properties.src) {
+        this.setData({
+          playing: true
+        })
+      }
+    },
+
+    _monitorSwitch: function() {
+      mMgr.onPlay(() => {
+        this._recoverStatus();
+      });
+
+      mMgr.onPause(() => {
+        this._recoverStatus();
+      });
+
+      mMgr.onStop(() => {
+        this._recoverStatus();
+      });
+
+      mMgr.onEnded(() => {
+        this._recoverStatus();
+      });
+    }
   }
 })
